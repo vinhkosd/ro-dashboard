b563f9467bb2502670976a91f8d651abf4cbf323
vinhkosd <vinh8c059@gmail.com>
2021-08-02 19:10:20 +0800

    renew config

--- app/config.php
+++ app/config.php
@@ -1,22 +1,22 @@
 <?php
     define('DB_CONNECTION', 'mysql');
-    define('DB_HOST', '149.28.140.143');
+    define('DB_HOST', '35.240.235.4');
     define('DB_PORT', '3306');
     define('DB_DATABASE', 'mygame');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', 'rogbt(*&%$#!@#98765');
+    define('DB_USERNAME', 'ro_webuser');
+    define('DB_PASSWORD', 'GBTeamEQRHRadaRADrokROM');
 
     define('DB_CONNECTION_GLOBAL', 'mysql');
-    define('DB_HOST_GLOBAL', '149.28.140.143');
+    define('DB_HOST_GLOBAL', '35.240.235.4');
     define('DB_PORT_GLOBAL', '3306');
     define('DB_DATABASE_GLOBAL', 'ro_global');
-    define('DB_USERNAME_GLOBAL', 'root');
-    define('DB_PASSWORD_GLOBAL', 'rogbt(*&%$#!@#98765');
+    define('DB_USERNAME_GLOBAL', 'ro_webuser');
+    define('DB_PASSWORD_GLOBAL', 'GBTeamEQRHRadaRADrokROM');
 
     define('DB_CONNECTION_GAME', 'mysql');
-    define('DB_HOST_GAME', '149.28.140.143');
+    define('DB_HOST_GAME', '35.240.235.4');
     define('DB_PORT_GAME', '3306');
     define('DB_DATABASE_GAME', 'ro_xd_r2');
-    define('DB_USERNAME_GAME', 'root');
-    define('DB_PASSWORD_GAME', 'rogbt(*&%$#!@#98765');
+    define('DB_USERNAME_GAME', 'ro_webuser');
+    define('DB_PASSWORD_GAME', 'GBTeamEQRHRadaRADrokROM');
 ?>
